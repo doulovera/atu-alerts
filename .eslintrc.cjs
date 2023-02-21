aa95e7451e876381083a05560ec81dee72b5163e
Douglas Lovera <doulovera@gmail.com>
2023-02-21 14:52:58 -0500

    start project

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+}
